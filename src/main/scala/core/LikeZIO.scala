48e5f6443c7b49b44840e18eea83851dad031227
s@lamandra <ussashok09@gmail.com>
2019-12-28 14:41:13 -0800

    3

--- src/main/scala/core/LikeZIO.scala
+++ src/main/scala/core/LikeZIO.scala
@@ -6,20 +6,47 @@ import scala.util.{Failure, Success, Try}
 
 final class LikeZIO[+A](logs: Seq[String], eitherOpt: Option[Either[Seq[Throwable], Option[A]]]) {
 
-  def addLog(log: String): LikeZIO[A] = this.copy(logs = log +: this.logs)
+  @inline def addLog(log: String): LikeZIO[A] = this.copy(logs = log +: this.logs)
 
-  def addLog(logs: Seq[String]): LikeZIO[A] = this.copy(logs = logs ++: this.logs)
+  @inline def addLog(logs: Seq[String]): LikeZIO[A] = this.copy(logs = logs ++: this.logs)
+
+  @inline def map[U](f: A => U): LikeZIO[U] = {
+    val resultEitherOpt = this.eitherOpt.map {
+      case Right(optValue) => Right(optValue.map(f))
+      case Left(errors) => Left(errors)
+    }
+    LikeZIO(logs = this.logs, eitherOpt = resultEitherOpt)
+  }
+
+  @inline def flatMap[U](f: A => LikeZIO[U]): LikeZIO[U] = this.map(f).flatten
+
+  @inline def flatten[U](implicit ev: A <:< LikeZIO[U]): LikeZIO[U] = {
+    val thisLogs = this.logs
+    val thisEitherOpt = this.eitherOpt
+
+    thisEitherOpt match {
+      case Some(value) => ???
+      case None => this.copy[U](eitherOpt = None)
+    }
+  }
+
+  @inline def filter(f: A => Boolean): LikeZIO[A] = {
+    val resultEitherOpt = this.eitherOpt.map {
+      case Right(optValue) => Right(optValue.filter(f))
+      case Left(errors) => Left(errors)
+    }
+    LikeZIO(logs = this.logs, eitherOpt = resultEitherOpt)
+  }
 
 
-
-  private def copy[U >: A](logs: Seq[String] = this.logs, eitherOpt: Option[Either[Seq[Throwable], Option[U]]] = this.eitherOpt): LikeZIO[U] = new LikeZIO(logs, eitherOpt)
+  @inline private def copy[U >: A](logs: Seq[String] = this.logs, eitherOpt: Option[Either[Seq[Throwable], Option[U]]] = this.eitherOpt): LikeZIO[U] = LikeZIO(logs, eitherOpt)
 }
 
 object LikeZIO {
 
-  final def fromLog(log: String): LikeZIO[Nothing] = new LikeZIO(logs = List(log), eitherOpt = None)
+  final def fromLog(log: String): LikeZIO[Nothing] = LikeZIO(logs = List(log), eitherOpt = None)
 
-  final def fromLog(logs: Seq[String]): LikeZIO[Nothing] = new LikeZIO(logs = logs, eitherOpt = None)
+  final def fromLog(logs: Seq[String]): LikeZIO[Nothing] = LikeZIO(logs = logs, eitherOpt = None)
 
 
   final def apply[T](value: => T): LikeZIO[T] = apply(Try(Option(value)))
@@ -29,12 +56,11 @@ object LikeZIO {
   final def apply[T](value: Try[T]): LikeZIO[T] = apply(value.map(Option(_)))
 
 
-  final def failed[T](exception: Throwable): LikeZIO[T] = new LikeZIO(logs = Nil, eitherOpt = Some(Left(List(exception))))
+  final def failed[T](exception: Throwable): LikeZIO[T] = LikeZIO(logs = Nil, eitherOpt = Some(Left(List(exception))))
 
-  final def failed[T](exceptions: List[Throwable]): LikeZIO[T] = new LikeZIO(logs = Nil, eitherOpt = Some(Left(exceptions)))
+  final def failed[T](exceptions: List[Throwable]): LikeZIO[T] = LikeZIO(logs = Nil, eitherOpt = Some(Left(exceptions)))
 
-  final def successful[T](result: T): LikeZIO[T] = new LikeZIO(logs = Nil, eitherOpt = Some(Right(Option(result))))
-
+  final def successful[T](result: T): LikeZIO[T] = LikeZIO(logs = Nil, eitherOpt = Some(Right(Option(result))))
 
   final private def apply[T](value: Try[Option[T]]): LikeZIO[T] = {
     val either = value match {
@@ -42,7 +68,11 @@ object LikeZIO {
       case Failure(exception) => Left(List(exception))
     }
 
-    new LikeZIO(logs = Nil, eitherOpt = Some(either))
+    LikeZIO(logs = Nil, eitherOpt = Some(either))
+  }
+
+  final private def apply[T](logs: Seq[String], eitherOpt: Option[Either[Seq[Throwable], Option[T]]]): LikeZIO[T] = {
+    new LikeZIO[T](logs, eitherOpt)
   }
 
 }
