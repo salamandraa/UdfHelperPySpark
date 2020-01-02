a5b7d5414749377def0de7488142322446a10037
s@lamandra <ussashok09@gmail.com>
2020-01-01 23:20:51 -0800

    Add test for udf with List[Int]

--- src/test/scala/SimpleUdfWithLikeZioSpec.scala
+++ src/test/scala/SimpleUdfWithLikeZioSpec.scala
@@ -2,10 +2,15 @@ import core.LikeZIO
 import org.apache.spark.sql.{Dataset, SparkSession}
 import org.scalatest._
 
+import scala.collection.mutable
+import scala.collection.parallel.immutable
+
 
 object SimpleUdfWithLikeZioSpec {
 
   case class Apple(id: Int, colour: String, size: Double)
+
+  case class AppleWareHouse(apples: List[Apple])
 
   def getApples(implicit spark: SparkSession): Dataset[Apple] = {
     import spark.implicits._
@@ -16,6 +21,20 @@ object SimpleUdfWithLikeZioSpec {
       Apple(3, "grey", 20.0),
       Apple(4, "red", 25.0)
     ).toDS()
+  }
+
+  def getAppleWareHouses(implicit spark: SparkSession): Dataset[AppleWareHouse] = {
+    import spark.implicits._
+    List(
+      AppleWareHouse(
+        List(
+          Apple(1, "red", 10.0),
+          Apple(2, "green", 15.0),
+          Apple(3, "grey", 20.0))
+      ),
+      AppleWareHouse(
+        List(Apple(4, "red", 25.0))
+      )).toDS()
   }
 
 }
@@ -47,10 +66,10 @@ class SimpleUdfWithLikeZioSpec extends FlatSpec with Matchers with SparkTest wit
     }
 
     applesIn.show()
-    val applesOut = applesIn.withColumn("add2", udfAdd2(col("id")))
+    val applesWithAdd2 = applesIn.withColumn("add2", udfAdd2(col("id")))
 
-    applesOut.printSchema()
-    applesOut.show(false)
+    applesWithAdd2.printSchema()
+    applesWithAdd2.show(false)
 
     val udfHyperbola = udf { value: Int =>
 
@@ -64,12 +83,34 @@ class SimpleUdfWithLikeZioSpec extends FlatSpec with Matchers with SparkTest wit
       result.prepareForSpark
     }
 
-    val applesOut2 = applesIn.withColumn("hyperbola", udfHyperbola(col("id")))
+    val applesWithHyperbola = applesIn.withColumn("hyperbola", udfHyperbola(col("id")))
 
-    applesOut2.printSchema()
-    applesOut2.show(false)
+    applesWithHyperbola.printSchema()
+    applesWithHyperbola.show(false)
 
 
+  }
+
+  it should "" in {
+    val appleWareHouses = getAppleWareHouses
+
+    val udfCount = udf { value: mutable.Seq[Apple] =>
+
+      implicit val likeZioLog: LikeZIO[Int] = LikeZIO(value.size, "Start")
+
+      val result = likeZioLog
+        .addLog("Calculated")
+        .addLog("End")
+
+
+      result.prepareForSpark
+    }
+
+    appleWareHouses.printSchema()
+    val appleWareHousesWithSize = appleWareHouses.withColumn("count", udfCount(col("apples")))
+
+    appleWareHousesWithSize.printSchema()
+    appleWareHousesWithSize.show(false)
   }
 
   after {
